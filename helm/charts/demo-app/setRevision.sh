814356ed5c0a374ef4430b3ad3608dab54fb960d
kirilc <kch@cybits.de>
2020-09-11 15:27:31 +0300

    Add full path url

--- helm/charts/demo-app/setRevision.sh
+++ helm/charts/demo-app/setRevision.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-sed -i "s/\${revision}/$1/g" ./helm/charts/demo-app/Chart.yaml
+sed -i "s/\${revision}/$1/g" /var/lib/jenkins/workspace/Deploy/Spring-Boot-Kubernetes/helm/charts/demo-app/Chart.yaml
